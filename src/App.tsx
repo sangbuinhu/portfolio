595c6d92d02578b3c1a479adea93d68d4e58e060
sangbuinhu <67570816+sangbuinhu@users.noreply.github.com>
2022-08-02 22:57:16 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,19 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {
-	BrowserRouter, Route, Routes
-} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import AppLayout from './components/layouts/AppLayout';
 import Home from './pages/Home';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<AppLayout />}>
 					<Route index element={<Home />} />
 				</Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	);
 }
 
